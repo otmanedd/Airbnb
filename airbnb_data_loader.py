69e911bce7eeee344a2d06d020377cefabac9c96
otmanedd <151662240+otmanedd@users.noreply.github.com>
2025-10-04 22:41:41 +0200

    Update airbnb_data_loader.py

--- airbnb_data_loader.py
+++ airbnb_data_loader.py
@@ -1,25 +1,18 @@
 import pandas as pd
-import gzip, shutil, os, requests
+import requests
+import os
 
 DATA_DIR = 'data'
 os.makedirs(DATA_DIR, exist_ok=True)
 
 URLS = {
-    'listings': 'http://data.insideairbnb.com/germany/hessen/frankfurt-am-main/2025-10-01/data/listings.csv.gz',
-    'reviews': 'http://data.insideairbnb.com/germany/hessen/frankfurt-am-main/2025-10-01/data/reviews.csv.gz',
-    'calendar': 'http://data.insideairbnb.com/germany/hessen/frankfurt-am-main/2025-10-01/data/calendar.csv.gz'
+    'listings': 'https://data.insideairbnb.com/germany/hessen/frankfurt-am-main/2025-10-01/data/listings.csv',
+    'reviews': 'https://data.insideairbnb.com/germany/hessen/frankfurt-am-main/2025-10-01/data/reviews.csv',
+    'calendar': 'https://data.insideairbnb.com/germany/hessen/frankfurt-am-main/2025-10-01/data/calendar.csv'
 }
 
-def download_and_extract(name, url):
-    local_gz = f'{DATA_DIR}/{name}.csv.gz'
-    local_csv = f'{DATA_DIR}/{name}.csv'
-    r = requests.get(url, stream=True)
-    with open(local_gz, 'wb') as f: shutil.copyfileobj(r.raw, f)
-    with gzip.open(local_gz, 'rb') as f_in:
-        with open(local_csv, 'wb') as f_out:
-            shutil.copyfileobj(f_in, f_out)
-    return local_csv
-
-for key in URLS:
-    download_and_extract(key, URLS[key])
-print("Daten Frankfurt heruntergeladen und entpackt!")
+for name, url in URLS.items():
+    r = requests.get(url)
+    with open(f'{DATA_DIR}/{name}.csv', 'wb') as f:
+        f.write(r.content)
+    print(f'{name} heruntergeladen!')
